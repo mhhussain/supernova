7646a02c1fd00fc6ed269e6456e516b9a6354132
moohh <mo051191@yahoo.com>
2019-08-12 23:54:18 -0400

    abstracted supernova

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-let axios = require('axios');
 let e =  require('express');
 
 let configs = require('./config');
 let health = require('./health');
+let supernova = require('./supernova');
 
 /// start up
-// particlemap
-let particlemap = {};
-let ptout = 0;
-let ptin = 0;
-
 let app = e();
 
 // uses
@@ -17,156 +12,7 @@ app.use(e.json());
 
 let h = new health(app);
 
-app.get('/requestmap', (req, res) =>  {
-    res.json({
-        pong: {
-            health: 'pong',
-            success: '/pong/s',
-            fail: '/pong/f',
-        }
-    })
-});
-
-app.get('/status/ptin', (req, res) => {
-    res.json(ptin);
-});
-
-app.get('/status/ptout', (req, res) => {
-    res.json(ptout);
-});
-
-app.get('/status/cid/:correlationId', (req, res) => {
-    let { correlationId } = req.params;
-    res.json(particlemap[correlationId]);
-});
-
-app.get('/particlemap', (req, res) => {
-    res.json(particlemap);
-});
-
-// entry point
-app.post('/particlemap/add', (req, res) => {
-    let { particleInfo } = req.body;
-    if (!particleInfo) {
-        res.status(400).json(new Error('contract missing: [particleInfo]'));
-        return;
-    }
-
-    particlemap[particleInfo.correlationId] = {
-        endpoint: particleInfo.endpoint,
-        bouncec: 0,
-        ptc: 0,
-        particlerepeater: particleInfo.particlerepeater
-    };
-
-    var success = true;
-
-    axios.post(configs.particleaccelerator, {
-        particle: {
-            endpoint: particleInfo.endpoint,
-            return: {
-                success: `${configs.novaurl}/pong/s`,
-                fail: `${configs.novaurl}/pong/f`
-            },
-            data: {
-                correlationId: particleInfo.correlationId
-            }
-        }
-    }).catch((err) => {
-        particlemap[particleInfo] = null;
-        success = false;
-        return;
-    }).finally(() => {
-        if (success) {
-            res.json('particle.fired');
-        } else {
-            res.status(400).json(new Error('failed to create particle'));
-        }
-        return;
-    });
-});
-
-app.post('/pong/s', (req, res) => {
-    if (h.status() != 'listening') {
-        res.status(503).json('bad health status');
-        return;
-    }
-
-    let { rdata } = req.body;
-    if (!rdata) {
-        res.json('particle.missing');
-        return;
-    }
-
-    let cid = rdata.correlationId;
-
-    // particle counter
-    ptin++;
-
-    // update particlemap
-    particlemap[cid].bouncec++;
-    particlemap[cid].ptc++;
-
-    // if particle is a fault
-    if (rdata.fault) {
-        res.json(`particle.recieved [${cid}]`);
-        return;
-    }
-
-    // create particle response
-    let particleconfig = particlemap[cid];
-
-    if (!particleconfig) {
-        res.status(400).json(new Error(`no particlemap for [${cid}]`));
-        return;
-    }
-
-    res.json(`particle.received [${cid}]`);
-
-    if (particleconfig.particlerepeater) {
-        for (var i = 0; i < particleconfig.particlerepeater; i++) {
-            axios.post(configs.particleaccelerator, {
-                particle: {
-                    endpoint: particleconfig.endpoint,
-                    return: {
-                        success: `${configs.novaurl}/pong/s`,
-                        fail: `${configs.novaurl}/pong/f`,
-                    },
-                    data: {
-                        correlationId: cid,
-                        fault: true
-                    }
-                }
-            });
-        }
-    }
-
-    axios.post(configs.particleaccelerator, {
-        particle: {
-            endpoint: particleconfig.endpoint,
-            return: {
-                success: `${configs.novaurl}/pong/s`,
-                fail: `${configs.novaurl}/pong/f`,
-            },
-            data: {
-                correlationId: cid,
-                fault: false
-            }
-        }
-    });
-
-    particlemap[cid].ptc += +particleconfig.particlerepeater;
-    particlemap[cid].ptc++;
-    ptout += +particleconfig.particlerepeater;
-    ptout++;
-
-    return;
-});
-
-app.post('/pong/f', (req, res) => {
-    // remove particle from map
-    res.json('failure.received');
-});
+new supernova(h, configs, app);
 
 app.listen(configs.port, () => { 
     h.updateStatus({
