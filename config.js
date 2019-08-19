ea3d000552da8d4edb465d203702350be3b9f2c8
moohh <mo051191@yahoo.com>
2019-08-19 14:21:48 -0400

    pulling configs from kube

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let configs = {
     port: 80,
-    novaurl: 'http://sunova',
-    particleaccelerator: 'http://partacc/outbox'
+    novaurl: `http://${process.env.SUNOVA_SERVICE_HOST}`,
+    particleaccelerator: `http://${process.env.PARTACC_SERVICE_HOST}/outbox`
 };
 
 module.exports = configs;
