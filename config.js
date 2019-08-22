087de67b1b7be83f045ebef27a6703fc8d3d7897
moohh <mo051191@yahoo.com>
2019-08-22 15:51:59 -0400

    mongo configs

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 let configs = {
     port: 80,
     novaurl: 'http://sunova',
-    particleaccelerator: 'http://partacc/outbox'
+    particleaccelerator: 'http://partacc/outbox',
+    mongo: {
+        url: 'mongodb://mongo/',
+        db: 'particleMap'
+    }
 };
 
 module.exports = configs;
